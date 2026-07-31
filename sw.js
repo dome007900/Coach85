f78fa932fffb71748299027a674b90cea0cdd3da
dome007900 <dome007900@gmail.com>
2026-07-31 15:46:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-self.addEventListener('fetch',e=>{});
+const CACHE='coach85-v1';
+const FILES=[
+ './',
+ './index.html',
+ './css/style.css',
+ './css/dashboard.css',
+ './js/app.js',
+ './js/dashboard.js'
+];
+
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
+self.addEventListener('fetch',e=>{
+ e.respondWith(
+   caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
